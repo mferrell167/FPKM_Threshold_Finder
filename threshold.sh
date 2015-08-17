b46a10a125e8b4828d6c241142da3e56b70302f2
mferrell167 <mferrell167@ufl.edu>
2015-08-17 11:45:19 -0500

    Update threshold.sh

--- threshold.sh
+++ threshold.sh
@@ -10,6 +10,7 @@ SCRIPTS=$1
 DIR=$2
 SCRIPT=${SCRIPTS}/find_false_positives.R
 SCRIPT2=${SCRIPTS}/clean.R
+SCRIPT3=${SCRIPTS}/clean_cufflinks_gtf.R
 A=$3
 
 cd $DIR
@@ -37,7 +38,7 @@ for file in $(ls ${DIR}); do
 	 if [ ${FORMAT} == 'xprs' ]; then
        grep "_" ${DIR}/${file} | cut -f 2,11-13 > ${DIR}/${file}_output/${file}_fpkm
 
-		cd ${DIR}/${file}_output
+       cd ${DIR}/${file}_output
 
        Rscript ${SCRIPT} ${DIR}/${file}_output/${file}_fpkm ${A}
 
@@ -45,6 +46,21 @@ for file in $(ls ${DIR}); do
 
 		cd ..
 	fi
+
+    if [ ${FORMAT} == 'gtf' ]; then
+        echo -e "ID\tFPKM\tFPKMlo\tFPKMhi" > ${DIR}/${file}_output/${file}_fpkm
+
+        cd ${DIR}/${file}_output
+
+awk '$3 == "transcript" {print $0}' ${DIR}/${file} |  cut -f 9  | cut -d ";" -f 2,3,5,6 | awk -F \" '{OFS = "\t"}; {print $2,$4,$6,$8}' | awk '$2 > 0 {print $0}' >> ${DIR}/${file}_output/${file}_fpkm
+
+        Rscript ${SCRIPT} ${DIR}/${file}_output/${file}_fpkm ${A}
+
+        cd ..
+
+    fi
+
+
 done
 
 
@@ -58,9 +74,7 @@ done
 
 mkdir ${DIR}/cleaned
 
-echo $(ls | grep -e "fpkm_tracking$" -e "xprs$")
-
-for f in $(ls | grep -e "fpkm_tracking$" -e "xprs$"); do
+for f in $(ls | grep -e "fpkm_tracking$" -e "xprs$" -e "gtf$"); do
 
     cd ${DIR}/cleaned
 
@@ -70,7 +84,16 @@ for f in $(ls | grep -e "fpkm_tracking$" -e "xprs$"); do
 
     fname="${nfile}_clean.${FORMAT}"
 
-    Rscript $SCRIPT2 ${DIR}/${f} ${DIR}/${f}_output/${f}_fpkm_include.txt > ${fname}
+    if [[ ${FORMAT} == 'xprs' || ${FORMAT} == 'fpkm_tracking' ]]; then
+
+        Rscript $SCRIPT2 ${DIR}/${f} ${DIR}/${f}_output/${f}_fpkm_include.txt > ${fname}
+
+
+    elif [ ${FORMAT} == 'gtf' ]; then
+
+        Rscript $SCRIPT3 ${DIR}/${f} ${DIR}/${f}_output/${f}_fpkm_include.txt > ${fname}
+
+    fi
 
     cd ..
 
